1a3549e4c5f7c75b8f4f2f960a2291246e9767de
NTtkachenko <nataly.tkachenko107@gmail.com>
2016-05-28 04:12:50 +0300

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,38 @@
+package week1.home;
+
+import java.util.Scanner;
+
+/**
+ * Created by Nataly on 24.05.2016.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int num1 = sc.nextInt();
+
+        System.out.println("Введите второе число:");
+        int num2 = sc.nextInt();
+
+        System.out.println("Введите третье число:");
+        int num3 = sc.nextInt();
+
+        if (num1 > num2 & num1 > num3) {
+            System.out.println("Наибольшее число №1: " + num1);
+        } else if (num1 < num2 & num1 < num3) {
+            System.out.println("Наименьшее число №1: " + num1);
+        }
+        if (num2 > num1 & num2 > num3) {
+            System.out.println("Наибольшее число №2: " + num2);
+        } else if (num2 < num1 & num2 < num3) {
+            System.out.println("Наименьшее число №2: " + num2);
+        }
+        if (num3 > num2 & num3 > num1) {
+            System.out.println("Наибольшее число №3: " + num3);
+        } else if (num3 < num2 & num3 < num1) {
+            System.out.println("Наименьшее число №3: " + num3);
+        }
+    }
+}
+
+ 
