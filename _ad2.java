1e79b1c014a170e5f5f027088fd43067a3d660e0
NTtkachenko <nataly.tkachenko107@gmail.com>
2016-05-28 04:20:09 +0300

    Create _ad2.java

---
+++ _ad2.java
@@ -0,0 +1,44 @@
+package week1.Addition;
+
+import java.util.Scanner;
+
+/**
+ * Created by Nataly on 27.05.2016.
+ */
+public class _ad2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int number1 = sc.nextInt();
+        int resultpow1 = number1 * number1 * number1;
+
+        System.out.println("Введите второе число:");
+        int number2 = sc.nextInt();
+        int resultpow2 = number2 * number2 * number2;
+
+        System.out.println("Введите третье число:");
+        int number3 = sc.nextInt();
+        int resultpow3 = number3 * number3 * number3;
+
+        if(number1 >= 0){
+            System.out.println("Первое число в кубе: " + resultpow1);
+        }
+        else if (number1 < 0) {
+            System.out.println("Обнуленное число 1 = " + 0);
+        }
+
+        if(number2 >= 0){
+            System.out.println("Второе число в кубе: " + resultpow2);
+        }
+        else if (number2 < 0) {
+            System.out.println("Обнуленное число 2 = " + 0);
+        }
+
+        if(number3 >= 0){
+            System.out.println("Третье число в кубе: " + resultpow3);
+        }
+        else if (number2 < 0) {
+            System.out.println("Обнуленное число 3 = " + 0);
+        }
+    }
+}
